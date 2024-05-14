9628471d7b0230a8142af73a682c6be1ca8b1093
Dilmer Valecillos <dilmervst@gmail.com>
2024-05-13 22:00:23 -0600

    Added a few sprites

--- Assets/Scripts/XRInteractions.cs
+++ Assets/Scripts/XRInteractions.cs
@@ -1,16 +1,36 @@
+using System;
 using TMPro;
 using UnityEngine;
 
 public class XRInteractions : MonoBehaviour
 {
     [SerializeField] private TextMeshPro toggleGamePokeLabel;
+
+    [SerializeField] private Transform activeSprite;
+    [SerializeField] private Transform inactiveSprite;
     
     public void Log(string x) => Debug.Log(x);
+
+    private void Awake()
+    {
+        activeSprite.gameObject.SetActive(true);
+        inactiveSprite.gameObject.SetActive(false);
+    }
 
     public void ToggleGate()
     {
         bool isGateClosed = GamePortalManager.Instance.ToggleGameArea();
-        if (isGateClosed) toggleGamePokeLabel.text = "Open Gate";
-        else toggleGamePokeLabel.text = "Close Gate";
+        if (isGateClosed)
+        {
+            toggleGamePokeLabel.text = "Open Gate";
+            activeSprite.gameObject.SetActive(true);
+            inactiveSprite.gameObject.SetActive(false);
+        }
+        else
+        {
+            toggleGamePokeLabel.text = "Close Gate";
+            activeSprite.gameObject.SetActive(false);
+            inactiveSprite.gameObject.SetActive(true);
+        }
     }
 }
